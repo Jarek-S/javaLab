3d95318bfb09a9695c7953779c231a392a48a865
Jarek-S <jareks@naszepolice.pl>
2014-05-04 18:00:36 +0200

    początek roboty sieciowej

--- src/obsluga/Backup.java
+++ src/obsluga/Backup.java
@@ -9,8 +9,6 @@ import java.io.File;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.InputMismatchException;
